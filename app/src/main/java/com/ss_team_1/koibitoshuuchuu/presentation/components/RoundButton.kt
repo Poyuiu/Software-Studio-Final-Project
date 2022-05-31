28c0e9083e320e4d1bb05daf279c523995221534
109062231 <asdof2214@outlook.com>
2022-05-31 12:30:01 +0800

    manifest fixed tmp

---
+++ app/src/main/java/com/ss_team_1/koibitoshuuchuu/presentation/components/RoundButton.kt
@@ -0,0 +1,19 @@
+package com.ss_team_1.koibitoshuuchuu.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun RoundButton() {
+    Box(modifier = Modifier.background(color = Color.Blue, shape = CircleShape))
+    {
+
+    }
+}
