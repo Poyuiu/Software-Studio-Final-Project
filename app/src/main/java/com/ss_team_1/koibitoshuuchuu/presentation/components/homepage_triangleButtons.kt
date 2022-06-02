1eb5c5d41c8024a553ccfc9f7484d8045d844f4d
ryanpan <ryanpan0302@gmail.com>
2022-06-02 22:59:48 +0800

    finish character switch button

--- app/src/main/java/com/ss_team_1/koibitoshuuchuu/presentation/components/homepage_triangleButtons.kt
+++ app/src/main/java/com/ss_team_1/koibitoshuuchuu/presentation/components/homepage_triangleButtons.kt
@@ -1,7 +1,9 @@
 package com.ss_team_1.koibitoshuuchuu.presentation.components
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,21 +11,32 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ss_team_1.koibitoshuuchuu.R
 
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun TrianglebuttonPreview() {
-    RoundedTriangle()
+    leftRoundedTriangle()
 
 }
 
 @Composable
-fun RoundedTriangle() {
+fun leftRoundedTriangle() {
 
-    Canvas(modifier = Modifier.size(60.dp)) {
+    Canvas(
+        modifier = Modifier.size(60.dp)
+            .clickable (
+                enabled = true,
+                onClickLabel = "Clickable left shift",
+                onClick = {
+                   /*TODO*/
+                }
+            )
+    ) {
         val trianglePath = Path().apply {
             val height = size.height
             val width = size.width
@@ -34,7 +47,6 @@ fun RoundedTriangle() {
         drawPath(
             trianglePath ,
             color =  Color(0xfff8bbd0)
-
         )
         val trianglePathStroke = Path().apply {
             val height = size.height
@@ -54,4 +66,55 @@ fun RoundedTriangle() {
         )
     }
 }
+
+@Composable
+fun rightRoundedTriangle() {
+
+    Canvas(
+        modifier = Modifier.size(60.dp)
+            .clickable (
+                enabled = true,
+                onClickLabel = "Clickable right shift",
+                onClick = {
+                    /*TODO*/
+                }
+            )
+    ) {
+        val trianglePath = Path().apply {
+            val height = size.height
+            val width = size.width
+            moveTo(width-3f, 3f)
+            lineTo((width/1.5f)-1.0f, height/(2.0f))
+            lineTo(width-2f, height-1.0f)
+        }
+        drawPath(
+            trianglePath ,
+            color =  Color(0xfff8bbd0)
+        )
+        val trianglePathStroke = Path().apply {
+            val height = size.height
+            val width = size.width
+            moveTo(width, 0f)
+            lineTo((width/1.5f), height/(2.0f))
+            lineTo(width, height)
+            lineTo(width, 0f)
+        }
+        drawPath(
+            trianglePathStroke ,
+            color =  Color.White,
+            style = Stroke(
+                width = 2.dp.toPx(),
+                pathEffect = PathEffect.cornerPathEffect(4.dp.toPx())
+            )
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun rightTrianglebuttonPreview() {
+    rightRoundedTriangle()
+
+}
+
 
