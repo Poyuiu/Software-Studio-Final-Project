0948913541b69f68cc83cee8cd5dd347f4def799
Howardkhh <howard.hou.fan@gmai.com>
2022-06-08 23:42:01 +0800

    character data class added level and intimacy needed methods

--- app/src/main/java/com/ss_team_1/koibitoshuuchuu/domain/model/Character.kt
+++ app/src/main/java/com/ss_team_1/koibitoshuuchuu/domain/model/Character.kt
@@ -3,4 +3,24 @@ package com.ss_team_1.koibitoshuuchuu.domain.model
 data class Character(
     val id: Int,
     val intimacy: Int
-)
+) {
+    fun level(): Int {
+        var intimacy_needed = 0
+        var level = -1
+        while (intimacy >= intimacy_needed) {
+            level++
+            intimacy_needed = ((level + 2) * (level + 1)) / 2 * 100
+        }
+        return level
+    }
+
+    fun intimacy_needed(): Int {
+        var intimacy_needed = 0
+        var level = -1
+        while (intimacy >= intimacy_needed) {
+            level++
+            intimacy_needed = ((level + 2) * (level + 1)) / 2 * 100
+        }
+        return intimacy_needed - intimacy
+    }
+}
