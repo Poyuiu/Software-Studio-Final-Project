01c34d21ff893a328e8dc9e9f69175a123cab8ca
ryanpan <ryanpan0302@gmail.com>
2022-06-08 23:41:21 +0800

    diallog

--- app/src/main/java/com/ss_team_1/koibitoshuuchuu/presentation/pages/ShopPage.kt
+++ app/src/main/java/com/ss_team_1/koibitoshuuchuu/presentation/pages/ShopPage.kt
@@ -1,8 +1,14 @@
 package com.ss_team_1.koibitoshuuchuu.presentation.pages
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -16,6 +22,7 @@ import com.ss_team_1.koibitoshuuchuu.presentation.components.*
 fun ShopPage(
     buyflag:Boolean
 ){
+    val openDialog = remember { mutableStateOf(false)  }
     Box(
         Modifier.fillMaxSize()
     ){
@@ -28,7 +35,14 @@ fun ShopPage(
         )
         NavigationBar(modifier = Modifier.align(Alignment.BottomCenter))
         Column(
-            modifier = Modifier.align(Alignment.TopCenter),
+            modifier = Modifier.align(Alignment.TopCenter)
+                .clickable (
+                    enabled = true,
+                    onClickLabel = "Welcomepage click",
+                    onClick = {
+                        openDialog.value = true
+                    }
+                ),
             horizontalAlignment = Alignment.CenterHorizontally
         ){
             Spacer(modifier = Modifier.height(32.dp))
@@ -60,6 +74,42 @@ fun ShopPage(
             ShopGiftGoodsRow0()
             Spacer(modifier = Modifier.height(36.dp))
             ShopGiftGoodsRow1()
+        }
+
+        if (openDialog.value) {
+
+            AlertDialog(
+                onDismissRequest = {
+                    // Dismiss the dialog when the user clicks outside the dialog or on the back
+                    // button. If you want to disable that functionality, simply use an empty
+                    // onCloseRequest.
+                    openDialog.value = false
+                },
+                title = {
+                    Text(text = "Dialog Title")
+                },
+                text = {
+                    Text("Here is a text ")
+                },
+                confirmButton = {
+                    Button(
+
+                        onClick = {
+                            openDialog.value = false
+                        }) {
+                        Text("This is the Confirm Button")
+                    }
+                },
+                dismissButton = {
+                    Button(
+
+                        onClick = {
+                            openDialog.value = false
+                        }) {
+                        Text("This is the dismiss Button")
+                    }
+                }
+            )
         }
     }
 }
