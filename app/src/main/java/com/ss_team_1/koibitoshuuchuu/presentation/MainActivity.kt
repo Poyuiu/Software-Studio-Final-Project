1254adb078e218642dcd9bab6c41372d49214f2e
poyu <yoyohihirobert@gmail.com>
2022-06-10 01:57:05 +0800

    navigation of more-button to userdatapage

--- app/src/main/java/com/ss_team_1/koibitoshuuchuu/presentation/MainActivity.kt
+++ app/src/main/java/com/ss_team_1/koibitoshuuchuu/presentation/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.ss_team_1.koibitoshuuchuu.presentation.components.PageProfile
+import com.ss_team_1.koibitoshuuchuu.presentation.components.UserDataPage
 import com.ss_team_1.koibitoshuuchuu.presentation.pages.*
 import com.ss_team_1.koibitoshuuchuu.ui.theme.KoiBitoShuuChuuTheme
 
@@ -25,6 +26,7 @@ class MainActivity : ComponentActivity() {
                     composable(Page.Welcome.route) { WelcomePage(navController/*...*/) }
                     composable(Page.Home.route) { HomePage(navController/*...*/) }
                     composable(Page.Profile.route) { PageProfile(navController) }
+                    composable(Page.UserData.route) { UserDataPage(navController) }
                     composable(Page.Shop.route) {
                         ShopPage(
                             navController = navController,
@@ -109,6 +111,7 @@ sealed class Page(val route: String) {
     object Welcome : Page("welcome_page")
     object Home : Page("home_page")
     object Profile : Page("profile_page")
+    object UserData : Page("user_data_page")
     object Shop : Page("shop_page")
     object FocusIntro : Page("focus_intro_page")
     object Focus : Page("focus_page")
