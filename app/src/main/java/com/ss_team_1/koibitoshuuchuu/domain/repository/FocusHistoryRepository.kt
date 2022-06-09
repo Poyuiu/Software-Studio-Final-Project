496e8fab21f244ec7e1dbd015ce7c73a27e94981
Howardkhh <howard.hou.fan@gmai.com>
2022-06-09 14:55:17 +0800

    change getLastWeekHistoryByCharacterIdGroupByDay return type to map

--- app/src/main/java/com/ss_team_1/koibitoshuuchuu/domain/repository/FocusHistoryRepository.kt
+++ app/src/main/java/com/ss_team_1/koibitoshuuchuu/domain/repository/FocusHistoryRepository.kt
@@ -1,13 +1,12 @@
 package com.ss_team_1.koibitoshuuchuu.domain.repository
 
 import androidx.lifecycle.LiveData
-import com.ss_team_1.koibitoshuuchuu.domain.model.FocusDailyHistory
 import com.ss_team_1.koibitoshuuchuu.domain.model.FocusHistory
 import java.util.*
 
 interface FocusHistoryRepository {
     fun getLastWeekHistoryByCharacterId(id: Int): LiveData<List<FocusHistory>>
-    fun getLastWeekHistoryByCharacterIdGroupByDay(id: Int): LiveData<List<FocusDailyHistory>>
+    fun getLastWeekHistoryByCharacterIdGroupByDay(id: Int): LiveData<Map<String, Float>>
     fun getAllHistory(): LiveData<List<FocusHistory>>
     fun insertHistory(characterId: Int, startTime: Calendar, endTime: Calendar)
 }
