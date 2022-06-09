93e0488586cb8b604d60c924937f2940f32a39af
ryanpan <ryanpan0302@gmail.com>
2022-06-09 15:18:17 +0800

    load data to homepage

--- app/src/main/java/com/ss_team_1/koibitoshuuchuu/presentation/pages/HomePage.kt
+++ app/src/main/java/com/ss_team_1/koibitoshuuchuu/presentation/pages/HomePage.kt
@@ -6,8 +6,10 @@ import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -17,6 +19,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.ss_team_1.koibitoshuuchuu.R
+import com.ss_team_1.koibitoshuuchuu.presentation.MyApplication
 import com.ss_team_1.koibitoshuuchuu.presentation.components.*
 
 @Preview
@@ -27,7 +30,36 @@ fun HomePage(
     Box(
         Modifier.fillMaxSize()
     ){
-        val intimacyLevel = 
+        val characterid: MutableState<Int> =
+            remember { mutableStateOf(0) }
+
+        //var checkedState by rememberSaveable { mutableStateOf(false) }
+        val intimacyLevel =
+            remember{
+                MyApplication.appContainer()
+                    .characterRepository.getCharacter(characterid.value)
+                    .value?.level() }
+
+        val intimacy =
+            remember{
+                MyApplication.appContainer()
+                    .characterRepository.getCharacter(characterid.value)
+                    .value?.intimacy }
+
+        val levelIntimacyNeed =
+            remember{
+                MyApplication.appContainer()
+                    .characterRepository.getCharacter(characterid.value)
+                    .value?.intimacyNeeded() }
+
+        val  showPercentage: MutableState<Boolean> =
+            remember { mutableStateOf(false) }
+
+        var lock = true
+        if (intimacyLevel != null) {
+            if ( intimacyLevel > 0 ) lock = false
+        }
+
         Image(
             painter = painterResource(id = R.drawable.coffee_shop_background),
             contentDescription = "",
@@ -42,14 +74,20 @@ fun HomePage(
             horizontalAlignment = Alignment.CenterHorizontally
         ){
             Spacer(modifier = Modifier.height(96.dp))
-            HomepageCharacter(2,700,1314,true, LocalContext.current,false ,0)
+            if (intimacyLevel != null && intimacy != null && levelIntimacyNeed !=null) {
+                HomepageCharacter(intimacyLevel,intimacy,levelIntimacyNeed,
+                    showPercentage.value, LocalContext.current, lock,characterid.value)
+            }else {
+                HomepageCharacter(100,5,10,
+                    showPercentage.value, LocalContext.current, lock,characterid.value)
+            }
         }
         Column(
             modifier = Modifier.align(Alignment.BottomCenter),
             horizontalAlignment = Alignment.CenterHorizontally
         ){
 
-            FocusButton(LocalContext.current,true)
+            FocusButton(LocalContext.current, lock)
             Spacer(modifier = Modifier.height(120.dp))
         }
         Row(
@@ -67,3 +105,4 @@ fun HomePage(
 
     }
 }
+
