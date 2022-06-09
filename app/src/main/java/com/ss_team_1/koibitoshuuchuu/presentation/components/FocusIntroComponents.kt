76a768105d305c92626e1c878ad0ad576232e9fd
109062231 <asdof2214@outlook.com>
2022-06-10 00:20:57 +0800

    time picker half

--- app/src/main/java/com/ss_team_1/koibitoshuuchuu/presentation/components/FocusIntroComponents.kt
+++ app/src/main/java/com/ss_team_1/koibitoshuuchuu/presentation/components/FocusIntroComponents.kt
@@ -11,9 +11,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
@@ -21,6 +19,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,35 +32,57 @@ import com.ss_team_1.koibitoshuuchuu.ui.theme.Secondary
 import com.ss_team_1.koibitoshuuchuu.ui.theme.huninnFamily
 import com.ss_team_1.koibitoshuuchuu.ui.theme.mamelonFamily
 import dev.chrisbanes.snapper.ExperimentalSnapperApi
+import dev.chrisbanes.snapper.LazyListSnapperLayoutInfo
+import dev.chrisbanes.snapper.rememberLazyListSnapperLayoutInfo
 import dev.chrisbanes.snapper.rememberSnapperFlingBehavior
 
+//TODO: Should be input
+val focusTimeList = (50 downTo 10 step 5).toList()
 
 @Preview(showBackground = false)
 @Composable
 private fun FocusIntroTimePickerPreview() {
-    FocusIntroTimePicker()
+    FocusIntroTimePicker(
+        lazyListState = rememberLazyListState(),
+        rememberLazyListSnapperLayoutInfo(lazyListState = rememberLazyListState())
+    )
 }
+
 
 @Composable
 fun FocusIntroTimePicker(
+    lazyListState: LazyListState,
+    layoutInfo: LazyListSnapperLayoutInfo
 ) {
-    //TODO: Should be input
-    val focusTimeList = (50 downTo 10 step 5).toList()
-    val lazyListState = rememberLazyListState()
-
     Surface(
         shape = RoundedCornerShape(17.dp),
         color = Primary,
         border = BorderStroke(width = 3.dp, color = Secondary),
-        modifier = Modifier.size(width = 279.dp, height = 308.dp)
+        modifier = Modifier
+            .size(width = 279.dp, height = 308.dp)
     ) {
+        Text(text = "${layoutInfo.currentItem?.index}", modifier = Modifier.padding(10.dp))
         LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(25.dp),
+            contentPadding = PaddingValues(vertical = 108.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
-            flingBehavior = rememberSnapperFlingBehavior(lazyListState)
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            state = lazyListState,
+            flingBehavior = rememberSnapperFlingBehavior(
+                lazyListState = lazyListState,
+                endContentPadding = 108.dp
+            )
         ) {
-            items(items = focusTimeList) { item ->
-                Text(text = "$item:00", fontSize = 36.sp, fontFamily = mamelonFamily)
+            itemsIndexed(items = focusTimeList) { index, item ->
+                val color = if (layoutInfo.currentItem?.index == index) Color.Black else Color.Gray
+                Text(
+                    text = "$item:00",
+                    fontSize = 36.sp,
+                    fontFamily = mamelonFamily,
+                    color = color
+                )
             }
         }
     }
@@ -71,11 +92,16 @@ fun FocusIntroTimePicker(
 @Preview
 @Composable
 private fun FocusIntroTimePickerButtonPreview() {
-    FocusIntroTimePickerButton()
+    FocusIntroTimePickerButton(
+        lazyListState = rememberLazyListState(),
+        rememberLazyListSnapperLayoutInfo(lazyListState = rememberLazyListState())
+    )
 }
 
 @Composable
 fun FocusIntroTimePickerButton(
+    lazyListState: LazyListState,
+    layoutInfo: LazyListSnapperLayoutInfo
 ) {
     var openState by remember { mutableStateOf(false) }
     var focusTime by remember { mutableStateOf(10) }
@@ -95,10 +121,11 @@ fun FocusIntroTimePickerButton(
         if (openState) {
             Popup(
                 alignment = Alignment.Center,
-                onDismissRequest = { openState = false },
+                onDismissRequest = { openState = false
+                                   focusTime = focusTimeList[layoutInfo.currentItem?.index!!]},
                 properties = PopupProperties()
             ) {
-                FocusIntroTimePicker()
+                FocusIntroTimePicker(lazyListState = lazyListState, layoutInfo = layoutInfo)
             }
         }
     }
@@ -137,13 +164,13 @@ fun FocusIntroWorkTextField(
 @Composable
 fun FocusIntroScenePicker(
 ) {
-    var openState by remember { mutableStateOf(false)}
+    var openState by remember { mutableStateOf(false) }
     var sceneName by remember { mutableStateOf("咖啡廳") }
     Column(modifier = Modifier.padding(12.dp)) {
         Text(text = "Scene")
         Button(
             modifier = Modifier.size(width = 280.dp, height = 64.dp),
-            onClick = { openState = true},
+            onClick = { openState = true },
             shape = RoundedCornerShape(17.dp),
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = Primary.copy(alpha = 0.7f)
@@ -152,10 +179,10 @@ fun FocusIntroScenePicker(
         ) {
             Text(text = sceneName, fontSize = 36.sp, fontFamily = huninnFamily)
         }
-        if (openState){
+        if (openState) {
             Popup(
                 alignment = Alignment.BottomCenter,
-                onDismissRequest = {openState = false}
+                onDismissRequest = { openState = false }
             ) {
                 Surface(
                     shape = RoundedCornerShape(15.dp),
