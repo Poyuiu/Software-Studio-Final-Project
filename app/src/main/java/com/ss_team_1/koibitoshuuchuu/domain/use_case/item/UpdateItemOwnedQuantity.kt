ccf6002d9e1deb02969d14bef76db42b4a6664f0
Howardkhh <howard.hou.fan@gmai.com>
2022-06-11 14:48:27 +0800

    fixed character and item not updating

--- app/src/main/java/com/ss_team_1/koibitoshuuchuu/domain/use_case/item/UpdateItemOwnedQuantity.kt
+++ app/src/main/java/com/ss_team_1/koibitoshuuchuu/domain/use_case/item/UpdateItemOwnedQuantity.kt
@@ -1,13 +1,13 @@
 package com.ss_team_1.koibitoshuuchuu.domain.use_case.item
 
 import com.ss_team_1.koibitoshuuchuu.domain.repository.ItemRepository
-import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.flow.first
 
 class UpdateItemOwnedQuantity(
     private val repository: ItemRepository
 ) {
     suspend operator fun invoke(id: Int, changeAmount: Int) {
-        val newQuantity = repository.getItem(id).last().quantity_owned + changeAmount
+        val newQuantity = repository.getItem(id).first().quantity_owned + changeAmount
         repository.setItemOwnedQuantity(id, newQuantity)
     }
 }
