61685df531218405d490f6ccc6fe3ff37eb7a988
poyu <yoyohihirobert@gmail.com>
2022-06-12 16:37:06 +0800

    pass on mac

--- app/src/main/java/com/ss_team_1/koibitoshuuchuu/presentation/pages/ProfilePage.kt
+++ app/src/main/java/com/ss_team_1/koibitoshuuchuu/presentation/pages/ProfilePage.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.ss_team_1.koibitoshuuchuu.R
 import com.ss_team_1.koibitoshuuchuu.presentation.MyApplication
