42effab37d86f4d2d9bb73f7f285a662b54c8084
Howardkhh <howard.hou.fan@gmai.com>
2022-06-13 02:25:24 +0800

    fixed conflict

--- app/src/main/java/com/ss_team_1/koibitoshuuchuu/presentation/components/UserDataChangeNamePopup.kt
+++ app/src/main/java/com/ss_team_1/koibitoshuuchuu/presentation/components/UserDataChangeNamePopup.kt
@@ -1,7 +1,6 @@
 package com.ss_team_1.koibitoshuuchuu.presentation.components
 
 
-import android.service.autofill.UserData
 import android.service.autofill.Validators.or
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -15,14 +14,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusManager
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
